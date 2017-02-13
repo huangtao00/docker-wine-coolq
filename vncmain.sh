0262a1a244b1330f36d43645b782181fe3b8d5ba
oott123 <git@public.oott123.com>
2017-02-13 19:29:23 +0800

    update vncmain to auto restart CQ

--- vncmain.sh
+++ vncmain.sh
@@ -1,5 +1,20 @@
-#!/usr/bin/with-contenv /bin/bash
+#!/bin/bash
 # Set them to empty is NOT SECURE but avoid them display in random logs.
 export VNC_PASSWD=''
 export USER_PASSWD=''
-wine ~/coolq/CQ*.exe /account $COOLQ_ACCOUNT || tail -n0 -f $0
+
+export TERM=linux
+
+while true; do
+    # 检查进程是否存在
+    process=`ps aux | grep 'CQ.\.exe'`
+    if [ -z $process ]; then
+        # 不存在则重启
+        wine ~/coolq/CQ?.exe /account $COOLQ_ACCOUNT
+        # 进程退出后等待 30 秒后再检查，避免 CQ 自重启导致误判
+        sleep 30
+    else
+        # 存在则说明是别的途径启动的，多等一会儿吧
+        sleep 100
+    fi
+done
